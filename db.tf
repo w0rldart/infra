2f5e21af58527aad297d0489e9dc3b8e7aee9605
Alex Budurovici <673081+w0rldart@users.noreply.github.com>
2021-10-28 18:26:32 +0200

    Create db and user

--- db.tf
+++ db.tf
@@ -1,10 +1,20 @@
 resource "digitalocean_database_cluster" "mysql" {
   name       = "${var.name_prefix}-mysql-cluster"
   engine     = "mysql"
-  version    = "11"
+  version    = "8"
   size       = "db-s-1vcpu-1gb"
   region     = var.region
   node_count = 1
 
   private_network_uuid = digitalocean_vpc.vpc.id
+}
+
+resource "digitalocean_database_db" "wordpress" {
+  cluster_id = digitalocean_database_cluster.mysql.id
+  name       = "${var.name_prefix}-wordpress"
+}
+
+resource "digitalocean_database_user" "user" {
+  cluster_id = digitalocean_database_cluster.mysql.id
+  name       = "${var.name_prefix}-user"
 }
